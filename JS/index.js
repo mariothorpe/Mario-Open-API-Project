a18be7ee9ac891e3153468c1ff5568879dbceb92
Jamarius Mario Thorpe <mariothorpe22@gmail.com>
2025-02-12 00:08:44 -0500

    final changes for open api project

--- JS/index.js
+++ JS/index.js
@@ -1,55 +1,41 @@
 const API_KEY="cfddce6b581600eb5347babeaea64b88"
-//fetch(`http://gateway.marvel.com/v1/public/characters?apikey=${API_KEY}&offset=1563&limit=1`)
-    //.then(response => response.json())
-    //.then((characters) => {
-        //console.log(characters);
-    //})
-    //.catch((error) => {
-        //console.log(error)
-    //});
 
-//fetch(`http://gateway.marvel.com/v1/public/characters/1017100?apikey=${API_KEY}`)
-    //.then(response => response.json())
-    //.then((character) => {
-        //console.log(character);
-    //})
-    //.catch((error) => {
-        //console.log(error)
-    //});
-
-//fetch(`http://gateway.marvel.com/v1/public/creators?apikey=${API_KEY}&limit=100`)
-    //.then(response => response.json())
-    //.then((creators) => {
-        //console.log(creators);
-    //})
-    //.catch((error) => {
-        //console.log(error)
-    //});
-
-
+    const bodyElement = document.getElementsByTagName("body");
     async function renderData() {
         const container = document.querySelector('.container');
-        const data = await fetch(`http://gateway.marvel.com/v1/public/characters?apikey=${API_KEY}&offset=1563&limit=1`);
+        const randomChar = Math.trunc(Math.random() * 1564);
+        const data = await fetch(`http://gateway.marvel.com/v1/public/characters?apikey=${API_KEY}&offset=${randomChar}&limit=1`);
         const characterData = await data.json();
                 console.log(characterData);
-    
+
         if (!data) {
             return;
         }
+
+        characterData.data.results.forEach(character => {
+            const card = document.getElementById("characterCard");
+            if (card.style.display === "none") {
+                card.style.display = '';
+            }
+
+            const name = document.getElementById("characterName");
+            name.textContent = "";
+            name.textContent = character.name;
+
+            const portrait = document.getElementById("characterPortrait");
+            portrait.src = "";
+            const imgSrcPath = `${character.thumbnail.path}/standard_fantastic.${character.thumbnail.extension}`;
+            portrait.src = imgSrcPath;
     
-        data.forEach(item => {
-            const card = document.createElement('div');
-            card.classList.add('card');
-    
-            const title = document.createElement('h2');
-            title.textContent = item.title;
-    
-            const body = document.createElement('p');
-            body.textContent = item.body;
-    
-            card.appendChild(title);
-            card.appendChild(body);
-            container.appendChild(card);
+            const description = document.getElementById('characterDescription');
+            description.textContent = character.description;
+
+            const comics = document.getElementById('characterComics');
+            comics.innerHTML = "";
+            const hasComics = character.comics.available > 0;
+            if (hasComics) {
+                comics.innerHTML = character.comics.items[0].name;
+            }
         });
     }
     renderData();
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Mario-Open-API-Project
+# Author: [Jamarius "Mario" Thorpe]
+
+Introduction
+------------
+I’m working on a project that involves displaying “character cards” for marvel characters using data from a marvel API. 
+
+Usage
+-----
+The website will generate a new “character card” when the “get random character” button is clicked. Each card should render data from the character name, character comic, character description and character image endpoints.
+
+Installation
+------------
+Clone repository (git clone “url”) in terminal and run using a live extension server.
