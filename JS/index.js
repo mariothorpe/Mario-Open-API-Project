2bf43ff6175f2540fb134d6726f27f524d5704f4
Jamarius Mario Thorpe <mariothorpe22@gmail.com>
2025-02-18 14:33:08 -0500

    second api fetch call added

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,19 @@
 const API_KEY="cfddce6b581600eb5347babeaea64b88"
 
     const bodyElement = document.getElementsByTagName("body");
+
     async function renderData() {
-        const container = document.querySelector('.container');
         const randomChar = Math.trunc(Math.random() * 1564);
         const data = await fetch(`http://gateway.marvel.com/v1/public/characters?apikey=${API_KEY}&offset=${randomChar}&limit=1`);
         const characterData = await data.json();
-                console.log(characterData);
+        console.log(characterData);
+       
 
         if (!data) {
             return;
         }
 
-        characterData.data.results.forEach(character => {
+        characterData.data.results.forEach(async character => {
             const card = document.getElementById("characterCard");
             if (card.style.display === "none") {
                 card.style.display = '';
@@ -30,12 +31,26 @@ const API_KEY="cfddce6b581600eb5347babeaea64b88"
             const description = document.getElementById('characterDescription');
             description.textContent = character.description;
 
+        
             const comics = document.getElementById('characterComics');
             comics.innerHTML = "";
+            const creatorH3 = document.getElementById('comicCreators');
+            creatorH3.innerHTML = "";
             const hasComics = character.comics.available > 0;
             if (hasComics) {
                 comics.innerHTML = character.comics.items[0].name;
+                comicURI = character.comics.items[0].resourceURI;
+                console.log(comicURI);
+                const comicResponse = await fetch(`${comicURI}?apikey=${API_KEY}`);
+                const comicData = await comicResponse.json();
+                console.log(comicData);
+                const comic = comicData.data.results[0];
+                const hasCreators = comic.creators.available > 0;
+                if (hasCreators) {
+                    creatorH3.innerHTML = `Creators: ${comic.creators.items.map(item => item.name).join(", ")}`;
+                }
             }
+        
         });
     }
     renderData();
